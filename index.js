9ba178be9f8ca879a166e0ea73d74778df46170b
Swapnil <swapu2002@gmail.com>
2022-02-07 14:49:50 +0530

    Refactored my code

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-// alert("Hey!");
-let numberOfDrums = document.querySelectorAll(".drum").length;
-for(let i=0;i<numberOfDrums;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        var buttonInnerHtML = this.innerHTML;
-        makeSound(buttonInnerHtML);
-    });
-}    
-    document.addEventListener("keypress",function(e){
-        makeSound(e.key);
-    });
+document.querySelector('.set').addEventListener('click', (e) => {
+    if(e.target.classList.contains('drum'))
+        makeSound(e.target.dataset.no);
+});
 
-function makeSound(key){
-    switch(key) {
-        case "1":
-            let audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-        case "2":
-            let audio2 = new Audio("sounds/tom-2.mp3");
-            audio2.play();
-            break;
-            case "3":
-            let audio3 = new Audio("sounds/tom-3.mp3");
-            audio3.play();
-            break;
-            case "4":
-            let audio4 = new Audio("sounds/tom-4.mp3");
-            audio4.play();
-            break;
-            case "5":
-            let audio5 = new Audio("sounds/kick-bass.mp3");
-            audio5.play();
-            break;
-            case "6":
-            let audio6 = new Audio("sounds/snare.mp3");
-            audio6.play();
-            break;
-            case "7":
-            let audio7 = new Audio("sounds/crash.mp3");
-            audio7.play();
-            break;
-        }
-}
+document.addEventListener("keypress", (e) => {
+    if(e && e.key >= "1" && e.key <= "7") makeSound(e.key);
+});
 
-
+const makeSound = (n) => new Audio(`sounds/tom-${n}.mp3`).play(); 
